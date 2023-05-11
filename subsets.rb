31aa7924cf1b932d15b7cfbcb7e5b7d341c70442
Purva Singh <singhpurva2906@gmail.com>
2023-05-11 16:55:27 -0400

    https://leetcode.com/problems/subsets/

---
+++ subsets.rb
@@ -0,0 +1,27 @@
+# @param {Integer[]} nums
+# @return {Integer[][]}
+
+def helper(idx, temp=[], res, nums)
+    if(idx >= nums.size)
+        res << temp
+        return
+    end
+    
+    # dont chose
+    helper(idx+1, temp, res, nums)
+    
+    # choose
+    helper(idx+1, temp+[nums[idx]], res, nums)
+    
+end
+
+def subsets(nums)
+#     temp = []
+#     res=[]
+    
+#     helper(0, temp, res, nums)
+#     res
+    res = [[]]
+    nums.each {|num| res += res.map {|arr| arr + [num]}}
+    res
+end
